1ec7c888bf6b258f3de0fc563eec9ca42238cb8b
Diegoslourenco <diego.lourenco15@gmail.com>
2022-05-15 22:45:41 -0300

    add acompanhar progresso

---
+++ frontend/controllers/AcompanharProgresso.js
@@ -0,0 +1,106 @@
+import { sendRequest } from "../utils/ApiUtils.js";
+
+class AcompanharProgresso {
+
+    constructor(acompanharProgresso) {
+
+        this.acompanharProgresso = document.getElementById(acompanharProgresso);
+
+        this.onLoad();
+    }
+
+    onLoad() { 
+
+        // setting the url
+        const url = "/inscricoes/" + localStorage.getItem('id_inscricao') + "/status";
+
+        // enviando a request e salvando a promise
+        const responsePromise = sendRequest('GET', url, "");
+
+        responsePromise.then(response => {
+
+            // log para debuggar
+            console.log(response);
+
+            if (response.status == 200) {
+                let rate = response.body / 5;
+
+                addContentProgresso(response.body, rate)
+                addContentRow(response.body);
+            }
+
+            function addContentProgresso(status, rate) {
+
+                const containerProgresso = document.getElementById("progresso");
+
+                // Construct content
+                const contentProgresso = `
+                <div class="progresso-titulo">
+                    <span>Meu progresso</span>
+                    <span>${status}/5</span>
+                </div>
+
+                <div class="jss958 progresso-barra root jss959 jss961" role="progressbar" aria-valuenow="67">
+                    <div class="jss968 jss969 linear-progress__bar root jss972" style="transform: scaleX(${rate});">
+                    </div>
+                </div>
+                `;
+            
+                // Append newyly created card element to the container
+                containerProgresso.innerHTML += contentProgresso;
+            }
+
+            function addContentRow(status) {
+
+                const containerRow = document.getElementById("row");
+
+                const contentRow = `
+                <div class="col-sm-1 col-md-6 side-content">
+                    <div class="bs-vertical">
+                    <ul>
+                        <li class="complete">
+                        <div class="texto">
+                            <h1>Candidatar à Vaga</h1>
+                        </div>
+                        <a href="#"><i class="ico fa fa-check ico-green"></i> </a>
+                        </li>
+                        <li class="current">
+                        <div class="texto">
+                            <h1>Validação da Inscrição</h1>
+                            <span>Estamos analisando sua inscrição. Por favor, aguarde.</span>
+                        </div>
+                        <a href="#"></a>
+                        </li>
+                        <li class="locked">
+                        <div class="texto">
+                            <h1>Teste</h1>
+                        </div> 
+                        <a href="#"><i class="ico fa fa-lock ico-muted"></i> </a>
+                        </li>
+                        <li class="locked">
+                        <div class="texto">
+                            <h1>Entrevista</h1>
+                        </div> 
+                        <a href="#"><i class="ico fa fa-lock ico-muted"></i> </a>
+                        </li>
+                        <li class="locked">
+                        <div class="texto">
+                            <h1>Resultado</h1>
+                        </div> 
+                        <a href="#"><i class="ico fa fa-lock ico-muted"></i> </a>
+                        </li> 
+                    </ul>
+                    </div>
+                </div>
+                `;
+            
+                // Append newyly created card element to the container
+                containerRow.innerHTML += contentRow;
+            }
+                
+        });
+
+    }
+}
+
+let acompanharProgresso = new AcompanharProgresso("acompanhar-progresso");
